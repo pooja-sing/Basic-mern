78356d91d61de56e62f4895a8852aeb7a3ec2227
pooja sing <poojasingh31280@gmail.com>
2023-07-28 12:18:34 +0530

    signup page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from "./components/Login";
+import React from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Signup from "./components/Signup";
+
+
+
+
 
 function App() {
+
+  const router = createBrowserRouter([
+   
+
+      
+    {
+      path: '/login',
+      element: <Login></Login>,
+    },
+    
+      
+    {
+      path: '/signup',
+      element: <Signup></Signup>,
+    },
+  
+  
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  
+   
+    <RouterProvider router={router} />
+ 
+   
     </div>
+
+
   );
 }
 
